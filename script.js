4035555d94d3d45c882f74ac3c897ef7cff49a10
Maribeth Calipo <maribethsc43@gmail.com>
2021-09-22 16:42:38 -0400

    Created script.js file.

---
+++ script.js
@@ -0,0 +1,41 @@
+// Const
+
+let num = 20;
+console.log(num);
+
+num = 15;
+console.log(num);
+
+num = num + 7;
+console.log(num);
+
+num--;
+console.log(num);
+
+num = "This is not a number"
+console.log(num);
+
+let blank;
+
+console.log(blank);
+
+blank = null;
+console.log(blank);
+
+
+bonus
+const quarter=0.25,dime=0.10,nickel=0.05,penny=0.01;
+
+console.log(0.25,0.10,0.05,0.01);
+
+let letters = "qwertyuiop";
+
+console.log(letters[letters.length - 1]);
+
+
+
+
+
+
+
+
